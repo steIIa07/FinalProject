85b342c3bc97e86bfdb0166e587074a4d467119a
steIIa07 <s1290087@u-aizu.ac.jp>
2024-05-31 01:16:45 +0900

    Add Scene Loader

--- Assets/Script/HitPointBar.cs
+++ Assets/Script/HitPointBar.cs
@@ -13,9 +13,10 @@ public class HitPointBar : MonoBehaviour
     }
     void Update()
     {
-        // HPバーがカメラの方向を向くようにする / Make the HP bar face the camera
+        //HPバーがカメラの方向を向くようにする / Make the HP bar face the camera
         var dir = Camera.main.transform.rotation;
         dir.z = 0;
+        dir.x = 0;
         transform.rotation = dir;
 
         // HPバーの値を敵のHPに合わせる / Set the value of the HP bar to match the _enemy's HP
--- Assets/Script/TreasureBox.cs
+++ Assets/Script/TreasureBox.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 public class TreasureBox : MonoBehaviour
 {
     private Animator _animator;
+    private AudioSource _audioSource;
     static readonly int Open = Animator.StringToHash("Open");
     private void Start() {
         _animator = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Weapon")) {
             _animator.SetTrigger(Open);
-            Destroy(gameObject, 10f);
+            _audioSource.Play();
+            Destroy(gameObject, 5f);
         }
     }
 }
